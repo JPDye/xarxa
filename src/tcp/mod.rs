d32883b670271e08ba74d665e1a5bce3420be4df
Joe Dye <jpzh.dye@gmail.com>
2026-08-06 17:29:24 +0100

    fix failing reordered pure ACK test by moving sequence number tracking after assembler decision

--- src/tcp/mod.rs
+++ src/tcp/mod.rs
@@ -1091,8 +1091,6 @@ impl TcpSocketState {
                     // the checks done above imply this.
                     debug_assert!(overlap_start <= overlap_end);
 
-                    self.local_rx_last_seq = Some(repr.seq_number);
-
                     (
                         &repr.payload[overlap_start - segment_start..overlap_end - segment_start],
                         overlap_start - window_start,
@@ -1464,6 +1462,9 @@ impl TcpSocketState {
             );
             return None;
         };
+
+        // assembler accepted segment, track sequence number for SACK generation
+        self.local_rx_last_seq = Some(repr.seq_number);
 
         // Place payload octets into the buffer.
         trace!(
